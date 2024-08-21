d7e14b78840c5c980d78f67c68e8e974e9bd6b0a
9JH1 <tkf.x1os@gmail.com.>
2024-08-21 23:56:37 +1200

    images + base

---
+++ scripts/land.js
@@ -0,0 +1,43 @@
+//landing page
+const landing_imageBackground = document.getElementById(
+  "landing-background-image"
+);
+const landing_imageBackgroundBackground = document.getElementById(
+  "landing-background-image-background"
+);
+fetch("../img/index.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    var count = Object.keys(data).length - 1;
+    var timing = 30000 + 10; //ms
+    // data and length ( count )
+    let image = "";
+    getNewImage();
+    function getNewImage() {
+      let imageTmp = `url(${
+        data[Math.floor(Math.random() * count)]["img_local"]
+      })`;
+      console.log("------");
+      console.log(imageTmp);
+      if (imageTmp == image) {
+        getNewImage();
+      } else {
+        image = imageTmp;
+      }
+    }
+    function setBackground() {
+      landing_imageBackground.style.backgroundImage = image;
+      landing_imageBackground.classList.remove("image-animation-call");
+      void landing_imageBackground.offsetWidth;
+      landing_imageBackground.classList.add("image-animation-call");
+      getNewImage();
+      landing_imageBackgroundBackground.style.backgroundImage = image;
+    }
+
+    setBackground();
+    setInterval(() => {
+      setBackground();
+    }, timing);
+  });
--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,3 @@
-//landing page 
-const landing_imageBackground = document.getElementById("landing-background-image");
-
-function landingPageImageAnimation() {
-    fetch("../img/index.json")
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            var count = (Object.keys(data).length)-1;
-            // data and length ( count ) 
-            // ;
-            // background-image: url(/img/banners/banner.png);
-            landing_imageBackground.style.backgroundImage
-            setInterval(()=>{
-                landing_imageBackground.style.backgroundImage = `url(${data[Math.floor(Math.random()*count)]["img_local"]})`
-            },12000)
-        })
-}
-
-landingPageImageAnimation()
+const searchResults = String(window.location.search)
+  .replace("?", "")
+  .replaceAll("%20", " ");
